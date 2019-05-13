dd88ccd8eda0ac58917436cadaee7486b35ca9ac
Gaurav Rai <gauravrai@tamu.edu>
2019-05-13 17:58:22 -0500

    Update Parsing_XML_file.py
    update code

--- Parsing_XML_file.py
+++ Parsing_XML_file.py
@@ -3,5 +3,8 @@
 Created on Mon May 13 17:53:16 2019
 
 @author: gaura
+
+The task here is to extract data from xml on authors of an article and add it to a list, one item for an author..
+The tags for first name, surname and email are mapped directly to the dictionary key.
 """
 
